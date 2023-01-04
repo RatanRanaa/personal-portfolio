30663307285ea4a24d2450f160caf3ed94e3cfa2
Ratan Rana <ratanrana206@gmail.com>
2023-01-04 10:08:51 +0530

    skills.js

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,9 +1,85 @@
-import React from 'react'
+import React from "react";
+
+import html from "../images/html.png";
+import css from "../images/css.png";
+import javascript from "../images/javascript.png";
+import reactImage from "../images/react.png";
+import nextjs from "../images/nextjs.png";
+import tailwind from "../images/tailwind.png";
+import graphql from "../images/graphql.png";
+import node from "../images/node.png";
+// import Image from "next/image";
 
 const Skills = () => {
-  return (
-    <div>Skills</div>
-  )
-}
+  const experiences = [
+    {
+      id: 1,
+      title: "HTML",
+      src: html,
+    },
+    {
+      id: 2,
+      title: "CSS",
+      src: css,
+    },
+    {
+      id: 3,
+      title: "JavaScript",
+      src: javascript,
+    },
+    {
+      id: 4,
+      title: "React",
+      src: reactImage,
+    },
+    {
+      id: 5,
+      title: "Next JS",
+      src: nextjs,
+    },
+    {
+      id: 6,
+      title: "Tailwind",
+      src: tailwind,
+    },
+    {
+      id: 7,
+      title: "GraphQL",
+      src: graphql,
+    },
+    {
+      id: 8,
+      title: "Node JS",
+      src: node,
+    },
+  ];
 
-export default Skills
+  return (
+    <div id="experience" className="w-full bg-black text-white h-full pb-16">
+      <div className="max-w-screen-xl mx-auto px-8 py-16 text-center md:text-left">
+        <h2 className="text-5xl md:text-7xl tracking-wider uppercase text-red-500 font-bold">
+          skills
+        </h2>
+        <p className="py-4 max-w-lg">
+          I have given too much time to learn this all thing just Now I have compleated One Years And Now a day's I am working on Next.js
+          
+        </p>
+
+        <div className="grid lg:grid-cols-4 gap-8">
+          {experiences.map(({ id, title, src }) => (
+            <div
+              key={id}
+              className="flex flex-col lg:flex-row gap-10 lg:gap-0 items-center justify-between p-6 shadow-lg rounded-xl hover:scale-105 ease-in duration-300
+              odd:shadow-rose-400 even:shadow-blue-400"
+            >
+              <img src={src} width="64px" height="64px" alt={title} />
+              <h3 className="font-light">{title}</h3>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
